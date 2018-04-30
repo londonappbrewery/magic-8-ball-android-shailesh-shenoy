994ab7e0ed38b754be036e4d0035c76a201fe16a
shailesh-shenoy <shenoy.shailesh97@gmail.com>
2018-04-30 23:16:22 +0530

    Completed with 5 choices: Yes, No, Ask again later, I have no idea, The answer is Yes.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,13 +1,44 @@
 package com.londonappbrewery.magiceightball;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button decideButton = findViewById(R.id.decideButton);
+
+        final ImageView ball = findViewById(R.id.ball);
+
+        final int[] ballImages = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5};
+
+        decideButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Random randomNumberGenerator = new Random();
+                int randomNumber = randomNumberGenerator.nextInt(5);
+                ball.setImageResource(ballImages[randomNumber]);
+                Log.d("magiceightball", "random number is " + randomNumber);
+            }
+        });
     }
 }
